40a91a0a772cbc302b9d78bac7ba93630022c5d1
HenryTsai <henry88002605@gmail.com>
2020-05-05 21:42:08 +0800

    user model and usercontroller

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,9 +1,10 @@
 class OmniauthController < ApplicationController
   def github 
-  @user = User.create_from_github_data(request.env['omniauth.auth'])
+  @user = User.create_from_provider_data(request.env['omniauth.auth'])
+  
     if @user.persisted?
-      sign_in_and_redirect @user
-      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
+      sign_in_and_redirect @user, :event => :authentication
+    #set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
     else
       flash[:error] = 'There was a problem signing you in through Github. Please register or try signing in later.'
       redirect_to new_user_registration_url
@@ -11,10 +12,11 @@ class OmniauthController < ApplicationController
   end
 
   def google_oauth2 
-  @user = User.create_from_github_data(request.env['omniauth.auth'])
+  @user = User.create_from_provider_data(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user
-      set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+    #set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+      
     else
       flash[:error] = 'There was a problem signing you in through Github. Please register or try signing in later.'
       redirect_to new_user_registration_url
@@ -27,3 +29,5 @@ class OmniauthController < ApplicationController
   end
 
 end
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,24 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:github, :google_oauth2]
 
+  # def self.create_from_provider_data(provider_data)
+  #   where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+  #     user.email = provider_data.info.email
+  #     user.password = Devise.friendly_token[0, 20]
+  #   end
+  # end
+
   def self.create_from_provider_data(provider_data)
-    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
-      user.email = provider_data.info.email
-      user.password = Devise.firendly_token[0, 20]
+    users = where(provider: provider_data.provider, uid: provider_data.uid)
+
+    if users.count == 0 && self.where(email: provider_data.info.email).count == 0
+      return User.create(email: provider_data.info.email, password: Devise.friendly_token[0, 20])
     end
+
+    if users.count == 0 && self.where(email: provider_data.info.email).count != 0
+      return self.where(email: provider_data.info.email).first
+    end
+    
+    return users.first
   end
 end
