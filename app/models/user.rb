b56226fc192b757539d85ff200a0b80803f8d449
HenryTsai <henry88002605@gmail.com>
2020-05-05 08:42:12 +0800

    set up user model with provider data and uid

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         :omniauthable, omniauth_providers: [:github, :google_oauth2]
+
+  def self.create_from_provider_data(provider_data)
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+      user.email = provider_data.info.email
+      user.password = Devise.firendly_token[0, 20]
+    end
+  end
 end
