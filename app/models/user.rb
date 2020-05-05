5c6f74ee3012b82f1ae0dee5f3f4db442e0d4ad6
HenryTsai <henry88002605@gmail.com>
2020-05-05 08:47:33 +0800

    generate omniauth controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:github, :google_oauth2]
 
   def self.create_from_provider_data(provider_data)
