0ccca91ec5f38f06acc8574d97dad71bf1ca0d38
Boyu <rider945@outlook.com>
2020-05-12 11:14:37 +0800

    WIP print resault(preview)

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,32 +6,30 @@ class CardsController < ApplicationController
   def create
     random_file = [*"a".."z", *"A".."Z"].sample(5).join('') + ".rb"
     tmp_file_path = Rails.root.join('tmp', "#{random_file}").to_s
-    test_data = 
-      params[:card][:answer].split(",\r\n").map{ |e|
-      e = "p " + e
-    }.join("\n")
+    test_data = params[:card][:answer].split(",\r\n").map{ |e| e = "p " + e }.join("\n")
     file = File.open(tmp_file_path, "w")
     file.write(params[:card][:default_code])
     file.write("\n")
     file.write(test_data)
     file.close
+    @result = `docker run -v #{tmp_file_path}:/#{random_file} ruby ruby /#{random_file}`
     puts "-----"
     puts `docker run -v #{tmp_file_path}:/#{random_file} ruby ruby /#{random_file}`.split("\n")
     puts "-----"
 
-
-    debugger
     @card = Board.find(params[:board_id]).cards.build(
       card_params.merge(
         level: `docker run -v #{tmp_file_path}:/#{random_file} ruby ruby /#{random_file}`.split("\n")
       )
     )
-    debugger
     if @card.save
-      redirect_to board_path(params[:board_id]), notice: 'create successfully!'
+      render :new
+      # redirect_to board_path(params[:board_id]), notice: 'create successfully!'
     else
       render :new
     end
+
+    File.unlink(tmp_file_path)
   end
 
   def edit
@@ -46,6 +44,12 @@ class CardsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @card = Board.find(params[:board_id]).cards.find_by(id: params[:id])
+    @card.destroy
+    redirect_to board_path(params[:board_id]), notice: 'deleted!'
   end
 
 
--- app/models/card.rb
+++ app/models/card.rb
@@ -2,5 +2,11 @@ class Card < ApplicationRecord
   belongs_to :board
 
   validates :title, presence: true
+  
+  default_scope -> { where(deleted_at: nil)}
+
+  def destroy
+    update(deleted_at: Time.now)
+  end
 
 end
