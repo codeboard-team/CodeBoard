0c54a7fdaf75c3fe534187dc94d76c24ea3016f8
Boyu <rider945@outlook.com>
2020-05-13 15:55:18 +0800

    WIP DockerRunByFile

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,30 +1,19 @@
 class CardsController < ApplicationController
+
+  def show
+  end
+
   def new
     @card = Card.new(test_code: [''])
   end
 
   def create
-    # random_file = [*"a".."z", *"A".."Z"].sample(5).join('') + ".rb"
-    # tmp_file_path = Rails.root.join('tmp', "#{random_file}").to_s
-    # test_data = params[:card][:test_code].split(",\r\n").map{ |e| e = "p " + e }.join("\n")
-    # file = File.open(tmp_file_path, "w")
-    # file.write(params[:card][:answer])
-    # file.write("\n")
-    # file.write(test_data)
-    # file.close
-    # result = `docker run -v #{tmp_file_path}:/#{random_file} ruby ruby /#{random_file}`
-    # File.unlink(tmp_file_path)
-    puts '------------------------'
-    puts "#{params[:card][:test_code]}"
-    puts '------------------------'
-    docker_run(params[:card][:answer], params[:card][:test_code])
-    puts '------------------------'
-    puts "#{params[:card][:test_code]}"
-    puts '------------------------'
+    @result = docker_run(params[:card][:answer], params[:card][:test_code]).split('======').pop
+    debugger
     @card = Board.find(params[:board_id]).cards.build(
       card_params.merge(
-        test_code: params[:card][:test_code],
-        result: @result.split("\n")
+        test_code: params[:card][:test_code].split(",\r\n"),
+        result: @result
       )
     )
     if @card.save
@@ -60,18 +49,24 @@ class CardsController < ApplicationController
   end
 
   private
+  def docker_detached()
+
+  end
+
   def docker_run(code, test_code)
     random_file = [*"a".."z", *"A".."Z"].sample(5).join('') + ".rb"
     tmp_file_path = Rails.root.join('tmp', "#{random_file}").to_s
-    test_data = test_code.split(",\r\n").map{ |e| e = "p " + e }.join(";\n")
+    test_data = test_code.split(",\r\n").map{ |e| e = "result.push(#{e})" }.join("\n")
     file = File.open(tmp_file_path, "w")
-    file.write(code)
-    file.write("\n")
-    file.write(test_data)
+    contents = [code,"require 'json'","result = []",test_data,"puts '======'","puts JSON.generate(result)"]
+    contents.each { |e|
+      file.write(e)
+      file.write("\n")
+    }
     file.close
-    @result = `docker run -v #{tmp_file_path}:/#{random_file} ruby ruby /#{random_file}`
+    result = `docker run -v #{tmp_file_path}:/#{random_file} ruby ruby /#{random_file}`
     # File.unlink(tmp_file_path)
-    return @result
+    return result
   end
 
   def card_params
