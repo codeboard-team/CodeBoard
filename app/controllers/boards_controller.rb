5b8f99e307d36068025ba9ce22e15d98c587314e
Boyu <rider945@outlook.com>
2020-05-15 18:12:29 +0800

    WIP check_authority

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,7 @@
 class BoardsController < ApplicationController
-
-  before_action :set_board, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:my]
+  before_action :check_authority, only: [:edit, :update, :destroy]
+  before_action :set_board, only: [:edit, :update, :destroy]
 
   def index
     @boards = Board.all
@@ -16,8 +17,9 @@ class BoardsController < ApplicationController
   end
 
   def create
-    @board = current_user.boards.new(board_params)
-
+    # @board = current_user.boards.new(board_params)
+    @board = Board.new(board_params.merge(user_id: current_user.id))
+    debugger
     if @board.save
       redirect_to my_boards_path, notice: 'create successfully!'
     else
@@ -26,13 +28,15 @@ class BoardsController < ApplicationController
   end
 
   def show
+    # @board = current_user.boards.find_by(id: params[:id])
+    @board = Board.find_by(id: params[:id])
   end
 
   def edit
   end
 
   def my
-    @board = Board.where(user_id: current_user)
+    @board = Board.where(user: current_user)
   end
 
   def update
@@ -51,8 +55,14 @@ class BoardsController < ApplicationController
   end
 
   private
+  def check_authority
+    redirect_to board_path(id: params[:id]) if Board.find_by(id: params[:id]).user_id != current_user.id
+  end
+  
   def set_board
-    @board = current_user.boards.find_by(id: params[:id])
+    # if Board.find_by(id: params[:id]).user_id == current_user.id
+    # @board = current_user.boards.find_by(id: params[:id])
+    @board = Board.find_by(id: params[:id])
   end
 
   def board_params
