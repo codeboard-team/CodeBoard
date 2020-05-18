4f784117070b71075dc1aefa6e209bfb8803a9a0
Boyu <rider945@outlook.com>
2020-05-18 17:41:55 +0800

    feature FixAuthority done

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,19 +1,19 @@
 class BoardsController < ApplicationController
   before_action :authenticate_user!, only: [:my, :new, :edit, :update, :destroy]
-  before_action :check_authority, only: [:new, :edit, :update, :destroy]
-  before_action :set_board, only: [:show, :edit, :update, :destroy]
+  before_action :find_board, only: [:show, :edit, :update, :destroy]
+  before_action :build_board, only: [:new, :create]
+  before_action :check_authority, only: [:edit, :update, :destroy]
 
   def index
     @board = Board.all
     @board = Board.page(params[:page]).per(6)
   end
 
-  def new
-    @board = Board.new
-  end
+  def new; end
 
   def create
-    @board = Board.new(board_params.merge(user_id: current_user.id))
+    @board.assign_attributes(board_params)
+
     if @board.save
       redirect_to board_path(@board.id), notice: 'create successfully!'
     else
@@ -21,38 +21,41 @@ class BoardsController < ApplicationController
     end
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def my
     @board = Board.where(user: current_user)
   end
 
   def update
-    @board.update(board_params)
-
-    if @board.save
+    if @board.update(board_params)
       redirect_to my_boards_path, notice: 'update successfully!'
     else
-      render :new
+      render :edit
     end
   end
 
   def destroy
-    @board.destroy
-    redirect_to my_boards_path, notice: 'deleted!'
+    if @board.destroy
+      redirect_to my_boards_path, notice: 'deleted!'
+    else
+      redirect_to my_boards_path
+    end
   end
 
   private
   def check_authority
-    redirect_to board_path(id: params[:id]) if Board.find_by(id: params[:id]).user_id != current_user.id
+    redirect_to board_path(id: @board.id), notice: 'check authority error! not owner!' if @board.user_id != current_user.id
   end
   
-  def set_board
+  def find_board
     @board = Board.find_by(id: params[:id])
+  end
+
+  def build_board
+    @board = current_user.boards.new
   end
 
   def board_params
