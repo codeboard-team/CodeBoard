2705a5461ce6aff3d699f1a2b8fbd4ce241e5aa6
lukeemail <kjd1760@gmail.com>
2020-05-27 14:22:58 +0800

    feat_personal_profile

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class SolutionsControllerTest < ActionDispatch::IntegrationTest
+class ProfilesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
