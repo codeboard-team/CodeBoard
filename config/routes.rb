fabd3311b5c7853bdda75d3f46f603832cf687ee
Boyu <rider945@outlook.com>
2020-05-30 17:32:55 +0800

    customize devise controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
 
-  devise_for :users, controllers: {omniauth_callbacks: 'omniauth'}
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', omniauth_callbacks: 'omniauth'}
   root to: 'home#index'
   
   resources :boards do
