25f20190fd4701b86101f2d31250223d58102c00
Boyu <rider945@outlook.com>
2020-06-04 12:37:09 +0800

    redirect to current page after Oauth sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
 
-  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', omniauth_callbacks: 'omniauth'}
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks'}
   root to: 'home#index'
   
   resources :boards do
