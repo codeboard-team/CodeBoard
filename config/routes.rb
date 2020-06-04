5776c24e9b6a4640cd6981e63c3d431ed7911f97
lukeemail <kjd1760@gmail.com>
2020-06-04 21:16:27 +0800

    fix_conflict

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,8 @@ Rails.application.routes.draw do
   end
 
   resources :cards, only: [:index] ,to: 'cards#list'
+  resources :cards
+  resources :profiles, only: [:show]
+  resources :cards, only: [:index] ,to: 'cards#list'
   
 end
