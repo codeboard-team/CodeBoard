467e3c880ee360ddf4678a1953b2c811fda57e51
lukeemail <kjd1760@gmail.com>
2020-06-07 16:24:13 +0800

    add_comments_relation

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,7 +4,7 @@ class Card < ApplicationRecord
   belongs_to :board
   has_many :records
   has_many :user, through: :records
-
+  has_many :comments
   validates :title, presence: true
   validates :level, presence: true
   validates :result, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :boards
   has_many :records
   has_many :cards, through: :records
-
+  has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
