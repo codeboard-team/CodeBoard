f8bf2c753c3d7dc04e2dd4dc7bcbb85f10573c94
lukeemail <kjd1760@gmail.com>
2020-06-08 21:51:41 +0800

    WIP3

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,6 @@ class User < ApplicationRecord
     )
   end
     end
-  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
