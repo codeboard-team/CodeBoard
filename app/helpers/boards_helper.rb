3fde69850320fc12d271ec4937797727485f2d12
HenryTsai <henry88002605@gmail.com>
2020-06-11 06:17:06 +0800

    moving board helper to application helper

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -1,14 +1,4 @@
 module BoardsHelper
-  # def language_color(language) 
-  #   case language.downcase
-  #     when language = 'ruby' 
-  #       'pink'
-  #     when language = 'javascript'
-  #       'yellow'
-  #     when language = 'python'
-  #       'blue'
-  #   end
-  # end
 end
 
 
