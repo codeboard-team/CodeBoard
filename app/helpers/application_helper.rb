cc551b3c3ac1d2935d76cc8c807979c5ed0509b2
HenryTsai <henry88002605@gmail.com>
2020-06-10 15:34:22 +0800

    moving language_color to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
     title ||= name.underscore.humanize
     inline_svg_tag(filename, aria: true, nocomment: true, title: title, class: styles)
   end
+
+  def language_color(language) 
+    case language.downcase
+      when language = 'ruby' 
+        'pink'
+      when language = 'javascript'
+        'yellow'
+      when language = 'python'
+        'blue'
+    end
+  end
+
 end
--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -1,2 +1,17 @@
 module BoardsHelper
+  # def language_color(language) 
+  #   case language.downcase
+  #     when language = 'ruby' 
+  #       'pink'
+  #     when language = 'javascript'
+  #       'yellow'
+  #     when language = 'python'
+  #       'blue'
+  #   end
+  # end
 end
+
+
+
+
+
