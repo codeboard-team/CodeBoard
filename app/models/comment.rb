3160eaccb7025625700b3cbc06454c3b06f940da
lukeemail <kjd1760@gmail.com>
2020-06-11 03:42:32 +0800

    create_polymorphic_relation

---
+++ app/models/comment.rb
@@ -0,0 +1,9 @@
+class Comment < ApplicationRecord
+  acts_as_paranoid
+  
+  belongs_to :user
+  belongs_to :commentable, :polymorphic => true
+  validates :content, presence: true, length: { minimum: 2 }
+
+  default_scope { where(deleted_at: nil).order(id: :desc) }
+end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,9 +1,8 @@
 class Record < ApplicationRecord
   belongs_to :card
   belongs_to :user
-  
+  belongs_to :commentable, :polymorphic => true
   # validates :code, presence: true
   
-   # Rcord.with_sloved
   scope :with_solved, -> { where(solved: true) }
 end
