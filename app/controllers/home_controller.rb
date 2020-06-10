8087ce04d127b72c8a8412dd21c43c23e9367fda
lukeemail <kjd1760@gmail.com>
2020-06-10 20:42:46 +0800

    add_omniauth_image_by_gravatar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,3 @@
 class HomeController < ApplicationController
-  def index
-    unless session["login_type"]
-      session["login_type"] = 'local'
-    end
-    puts session["login_type"]
-  end
+  
 end
