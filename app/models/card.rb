65ec42f271ab8b0cd4d9873aef79887549e111a1
lukeemail <kjd1760@gmail.com>
2020-06-11 02:30:57 +0800

    modify_tags_name

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,9 @@
 class Card < ApplicationRecord
-  TAGS = %w[ALGORITHMS ARRAYS BIONARY STRINGS SYMBOL HASH OBJECT NUMBERS RULES LOOPS UTILITIES MATHEMATICS LISTS DATA_STRCTURES ...].freeze
+  TAGS = %w[Algorithms Arrays Bionary Data_Strctures Strings Symbol Hash Object Numbers Rules Loops Utilities Mathematics Methods While If...else Lists].freeze
 
   belongs_to :board
   has_many :records
   has_many :user, through: :records
-
   validates :title, presence: true
   validates :level, presence: true
   validates :result, presence: true
