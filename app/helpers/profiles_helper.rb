0843a74e1cbd08a91d84bf13ef593beda49f7653
eudora-hsj <eudora.hsj@gmail.com>
2020-06-11 00:01:02 +0800

    修正圖片存入尺寸與navbar的user-photo顯示尺寸

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,5 +1,5 @@
 module ProfilesHelper
   def avatar_image_tag(user)
-    image_tag("https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email.strip.downcase)},",size: "40")
+    image_tag("https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email.strip.downcase)},",size: "250")
   end
 end
