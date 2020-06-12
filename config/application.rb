c946f20c4f95c3d5bd394b70ad01b31a044f3458
HenryTsai <henry88002605@gmail.com>
2020-06-12 22:20:43 +0800

    remove ngro

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,5 @@ module CodeBoard
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.hosts<< '60724fbc3411.ngrok.io'
   end
 end
